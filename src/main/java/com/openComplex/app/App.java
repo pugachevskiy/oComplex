ad6325d047b3080bd0c06b105ded344983182cc3
Matthias Fuchs <fuchsmat@fim.uni-passau.de>
2015-08-25 17:25:17 +0200

    Testing

--- src/main/java/com/openComplex/app/App.java
+++ src/main/java/com/openComplex/app/App.java
@@ -3,7 +3,7 @@ package com.openComplex.app;
 import com.openComplex.app.mainWindow.View.MainView;
 
 import javax.swing.*;
-//Test ab
+//Test abc
 
 public class App {
     public static MainView gui;
