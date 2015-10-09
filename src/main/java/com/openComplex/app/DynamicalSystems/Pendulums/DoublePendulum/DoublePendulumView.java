a6294e0a832db3b8f0e228d6b3daf663f3cd7564
pugachevskiy <s.pugachevskiy@gmail.com>
2015-10-09 13:22:53 +0200

    Abstract class for Model and View.

--- src/main/java/com/openComplex/app/DynamicalSystems/Pendulums/DoublePendulum/DoublePendulumView.java
+++ src/main/java/com/openComplex/app/DynamicalSystems/Pendulums/DoublePendulum/DoublePendulumView.java
@@ -1,7 +1,7 @@
 package com.openComplex.app.DynamicalSystems.Pendulums.DoublePendulum;
 
-import javax.swing.*;
-import java.awt.*;
+import com.openComplex.app.DynamicalSystems.Pendulums.PendulumsView;
+
 import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.List;
@@ -9,49 +9,17 @@ import java.util.List;
 /**
  * Created by strange on 08/10/15.
  */
-public class DoublePendulumView {
-    private static final int Lx = 500, Ly = 330; //graphic window
-    private JPanel pan;
-
+public class DoublePendulumView extends PendulumsView {
     private List<String> buttonsName = Arrays.asList("New", "Start", "Phi1 +", "Phi1 -", "Phi2 +", "Phi2 -", "Friction ++",
             "Friction --", "m1 +", "m1 -", "m2 +", "m2 -");
-    private List<JButton> buttons = new LinkedList<>();
-    private ActionListener listener;
-    private JFrame frame;
+
+
+    private static String NAME = "Double Pendulum";
+
 
     public DoublePendulumView(ActionListener listener) {
-        this.listener = listener;
-        init();
+
+        init(NAME, listener, buttonsName);
     }
 
-    public void init() {
-
-        frame = new JFrame("Double Pendulum");
-        frame.setSize(1100, 450);
-        frame.setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-        pan = new JPanel();
-        pan.setBounds(Lx + 1, 1, 100, Ly + 90);
-        pan.setBackground(new Color(0, 200, 100));
-        pan.setFont(new Font("Verdana", Font.PLAIN, 10));
-
-        addButtons();
-        frame.add(pan, BorderLayout.NORTH);
-        frame.setVisible(true);
-
-    }//init()
-
-    private void addButtons() {
-        for (int i = 0; i < buttonsName.size(); i++) {
-            this.buttons.add(new JButton(buttonsName.get(i)));
-            this.buttons.get(i).setActionCommand(buttonsName.get(i));
-            this.buttons.get(i).addActionListener(listener);
-            pan.add(this.buttons.get(i));
-        }
-    }
-
-    public void addPanel(JPanel panel) {
-        frame.add(panel, BorderLayout.CENTER);
-    }
 }
---
+++ src/main/java/com/openComplex/app/DynamicalSystems/Pendulums/PendulumsView.java
@@ -0,0 +1,48 @@
+package com.openComplex.app.DynamicalSystems.Pendulums;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
+
+/**
+ * Created by strange on 09/10/15.
+ */
+public abstract class PendulumsView {
+    private static final int Lx = 500, Ly = 330; //graphic window
+    private JFrame frame;
+    private JPanel pan;
+    private java.util.List<JButton> buttons = new LinkedList<>();
+
+    public void init(String name, ActionListener listener, List<String> list) {
+
+        frame = new JFrame(name);
+        frame.setSize(1100, 450);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        pan = new JPanel();
+        pan.setBounds(Lx + 1, 1, 100, Ly + 90);
+        pan.setBackground(new Color(0, 200, 100));
+        pan.setFont(new Font("Verdana", Font.PLAIN, 10));
+
+        addButtons(listener, list);
+        frame.add(pan, BorderLayout.NORTH);
+        frame.setVisible(true);
+
+    }//init()
+
+    private void addButtons(ActionListener listener, List<String> buttonsName) {
+        for (int i = 0; i < buttonsName.size(); i++) {
+            this.buttons.add(new JButton(buttonsName.get(i)));
+            this.buttons.get(i).setActionCommand(buttonsName.get(i));
+            this.buttons.get(i).addActionListener(listener);
+            pan.add(this.buttons.get(i));
+        }
+    }
+
+    public void addPanel(JPanel panel) {
+        frame.add(panel, BorderLayout.CENTER);
+    }
+}
